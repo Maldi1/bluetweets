00eef55e22ced5f626ef392a28681d1040025832
Malik <invers.uuk@gmail.com>
2020-01-11 19:38:58 +0700

    scheduler to run sentiment process

--- src/main/java/com/djinggoo/bigdataanalytic/bluetweets/service/SentimentService.java
+++ src/main/java/com/djinggoo/bigdataanalytic/bluetweets/service/SentimentService.java
@@ -3,12 +3,17 @@ package com.djinggoo.bigdataanalytic.bluetweets.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.djinggoo.bigdataanalytic.bluetweets.model.Sentiment;
 import com.djinggoo.bigdataanalytic.bluetweets.repository.SentimentRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class SentimentService {
 	
     @Autowired private SentimentRepository sentimentRepository;
@@ -20,5 +25,21 @@ public class SentimentService {
     public List<Sentiment> getSentiments(){
         return sentimentRepository.findAll();
     }
+    
+	@Scheduled(fixedRate = 1200000) //running tiap 20 menit
+	public void removedata() {
+		
+//	    final String uri = "https://bluetweets.herokuapp.com/";
+	    final String uri = "http://localhost:5000/sentiment/process"; // flask sentiment process
+
+	    try {
+		    RestTemplate restTemplate = new RestTemplate();
+		    String result = restTemplate.getForObject(uri, String.class);
+		    log.info("result : "+result);
+	    }catch (Exception e) {
+			log.error(uri + " >>> connection refused !!");
+		}
+
+	}
 
 }
